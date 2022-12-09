e1d4bedceaf01a788c21f0a41512ea61e70722fc
Stas Chernov <schernovmail@yandex.ru>
2022-12-09 09:46:13 +0300

    Fix: cors

--- backend/app.js
+++ backend/app.js
@@ -14,15 +14,16 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env;
 const app = express();
 
-// app.use(cors);
+//app.use(cors);
 // app.use(helmet());
 
-app.use(
-  cors({
-    origin: 'https://memesto.nomoredomains.club',
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: '*',
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+  ],
+}));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
