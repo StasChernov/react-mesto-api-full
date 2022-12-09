89ebf5de36ff9df646d1d27261425798c11cee19
Stas Chernov <schernovmail@yandex.ru>
2022-12-09 10:09:22 +0300

    Fix: cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
@@ -8,22 +9,17 @@ const auth = require('./middlewares/auth');
 const handlerErrors = require('./middlewares/handlerErrors');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-//app.use(cors);
-// app.use(helmet());
-
-app.use(cors({
-  origin: '*',
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-  ],
-}));
+app.use(cors(
+  {
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  },
+));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
